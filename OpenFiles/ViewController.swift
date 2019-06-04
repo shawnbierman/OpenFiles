6aaf63a7ecfbbb727a6f5e44828f5b4636bb792c
Shawn Bierman <shawnbierman@icloud.com>
2019-06-03 19:11:54 -0500

    Initial Commit

---
+++ OpenFiles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenFiles
+//
+//  Created by Shawn Bierman on 6/3/19.
+//  Copyright © 2019 Shawn Bierman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
